27d4242301010ee7f5dcb750ce32070ed6d847dd
Ndibechinonso <edwardndibe@gmail.com>
2021-05-22 10:14:54 +0100

    error btn

--- src/components/DataLoad/DataLoad.js
+++ src/components/DataLoad/DataLoad.js
@@ -1,46 +1,59 @@
-import React, { useEffect, useContext } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { useLocation } from 'react-router-dom';
-import './DataLoad.css'
-import {fetchUsers} from '../../redux'
-import { Link } from 'react-router-dom'
-import DotRing from '../DotRing/DotRing'
-import { MouseContext } from '../../context/mouse-context'
-
+import React, { useEffect, useContext } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
+import "./DataLoad.css";
+import { fetchUsers } from "../../redux";
+import { Link } from "react-router-dom";
+import DotRing from "../DotRing/DotRing";
+import { MouseContext } from "../../context/mouse-context";
+import Button from '../Button/Button'
 
 const DataLoad = (props) => {
-    const { cursorType, cursorChangeHandler } = useContext(MouseContext);
-    const loader = useSelector(state => state.userData.loading)
-    const data = useSelector(state => state.userData.data)
-    const error = useSelector(state => state.userData.error)
-   
-    const dispatch = useDispatch()
-    useEffect(()=>{
+  const { cursorType, cursorChangeHandler } = useContext(MouseContext);
+  const loader = useSelector((state) => state.userData.loading);
+  const userData = useSelector(state => state.userData.data[0])
+  const error = useSelector((state) => state.userData.error);
 
-        dispatch(fetchUsers())
-     
-    },[])
+ 
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
 
-    const location = useLocation();
+  const location = useLocation();
 
-useEffect(() => {
+  useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
-    const code = searchParams.get('code')
-    localStorage.setItem('code', code)
-}, []);
+    const code = searchParams.get("code");
+    localStorage.setItem("code", code);
+  }, []);
 
-return(
+
+
+  return (
     <div>
-<DotRing />
-<div
-          onMouseEnter={() => cursorChangeHandler("hovered")}
-          onMouseLeave={() => cursorChangeHandler("")}
-        >
-    <div className='playDiv'> {data.length !== 0 && error.length === 0 ? <div className='playCircleActing'><Link to='/home'><a><i className="fas play fa-play"></i></a></Link></div> : <div className='playCircle'><div className="hourglass"></div></div>} </div>
+      <DotRing />
+      <div
+        onMouseEnter={() => cursorChangeHandler("hovered")}
+        onMouseLeave={() => cursorChangeHandler("")}
+      >
+        <div className="playDiv">
+          {" "}
+          {!loader ? userData && !userData.id ? <div className='errorDiv'> <div className='errorMsg'>Something went terribly wrong. Please click the button below to take you back to the Login Page </div><div><Link to='/'><Button className='navBarButton' text='Go back to Login Page' /></Link></div></div> : (
+            <div className="playCircleActing">
+              <Link to="/home">
+                  <i className="fas play fa-play"></i>
+              </Link>
+            </div>
+          ) : (
+            <div className="playCircle">
+              <div className="hourglass"></div>
+            </div>
+          )}{" "}
+        </div>
+      </div>
     </div>
-    </div>
-)
-
-}
+  );
+};
 
 export default DataLoad;
--- src/components/Playlists/Playlists.js
+++ src/components/Playlists/Playlists.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './Playlists.css'
 import { useSelector, useDispatch } from 'react-redux'
 import LoggedInNav from '../LoggedInNav'
@@ -6,101 +6,116 @@ import AOS from 'aos'
 import 'aos/dist/aos.css';
 import Modal from '../Modal/Modal'
 import axios from 'axios'
-import {fetchPlaylist} from '../../redux'
+import { fetchPlaylist } from '../../redux'
 import play from '../../Assets/play.png'
 import playlistLogo from '../../Assets/playlistLogo.png'
 import cancelButton from '../../Assets/cancelButton.png'
 import deleteButton from '../../Assets/deleteButton.png'
-import twnd from '../../Assets/twnd.png'
 
 
 const Playlists = (props) => {
+
     const dispatch = useDispatch()
     const playlistData = useSelector(state => state.playlist.data)
     const loader = useSelector(state => state.playlist.loading)
     console.log(loader, 'playlistData')
     const [show, setShow] = useState(false)
     const [select, setSelect] = useState('')
-const[tracklist, settracklist] = useState(null)
+    const [tracklist, settracklist] = useState(null)
 
+    const [playTrack, setPlayTrack] = useState(false)
+    console.log(playTrack, 'playTrack')
+    const [trackId, setTrackId] = useState('')
 
-    useEffect(()=>{
+    const audioTune = new Audio(`${trackId}`);
+    console.log(trackId, 'trackid')
+    useEffect(() => {
+        audioTune.load();
+    }, [])
+
+    const player = () => {
+
+        const playSound = () => {
+            audioTune.play();
+            setPlayTrack(true)
+        }
+
+        // pause audio sound
+        const pauseSound = () => {
+            audioTune.pause();
+            setPlayTrack(false)
+        }
+
+        playTrack ? pauseSound() : playSound()
+    }
+
+    useEffect(() => {
         AOS.init({
             duration: 1000
         })
     })
 
-    useEffect(async()=>{
-        if(tracklist){
+    useEffect(async () => {
+        if (tracklist) {
             console.log(tracklist, 'tracklist')
             localStorage.setItem('playlistUrl', tracklist)
-        // const result = await axios({tracklist})
-
-        // console.log(result.data, 'result')
-        // setList(result.data)
-        // console.log(list, 'list')
-    }
+        }
     })
-      
-        const fetchedData = useSelector(state => state.userData.data[9]) 
-        const fetchedData2 = useSelector(state => state.userData.data[10]) 
 
- 
- if (fetchedData){
-    var playlists = fetchedData.data
- 
-}
+    const fetchedData = useSelector(state => state.userData.data[9])
+    const fetchedData2 = useSelector(state => state.userData.data[10])
 
-if (fetchedData2){
-    var recomplaylists = fetchedData2.data
-   
-}
+    if (fetchedData) {
+        var playlists = fetchedData.data
+    }
 
+    if (fetchedData2) {
+        var recomplaylists = fetchedData2.data
+    }
 
-   
-return (
-    <div className='artistsContainer'>
-        <LoggedInNav />
-        <div className='artistsBody'>
-        <div class='header'>Playlists</div>
-        <div className='topArtistsContainer'>
-            { playlists && playlists.map(playlist =>{
-        return(
-        <div key={playlist.id}>
-<Modal show={show} onClose={()=>setShow(false)} title='Playlists'>
-    <div className='closeBtn'><div onClick={() => setShow(false)} className='closeContainer'><img className='closeImg' src={cancelButton} /></div></div>
-<div className='playlistModal-title'>{select}</div>
-<div className='playlistModal-createDiv'>buttons</div>
- {!loader ? playlistData.map(track=>{
-    return(
-        <div key={track.id} className='childrenContainer'>
-    
-
-<div className='playlistModal-body'>
-<div className='modalListDiv'><img className='deleteImg' src={deleteButton} /><img className='twndimg' src={track.artist.picture_xl}/></div> <div className='playListModalTittle'><div>{track.title}</div> <div>{track.artist.name}</div></div> <div className='playlistPlayDiv'><img className='playImg' src={play} /> <img className='playlistLogoImg' src={playlistLogo} /></div>
-
-</div>
-</div>
-    )
-})  : <div className='spinnerContainer'> <div className="lds-facebook"><div></div><div></div><div></div></div> </div>}
- 
-</Modal>
-        <div data-aos='fade-up' className='artistImgContainer'  onClick={()=>{setShow(true); setSelect(playlist.title); settracklist(playlist.tracklist); dispatch(fetchPlaylist(playlist.tracklist))}}>
-            <img src={playlist.picture_xl} alt='' />
-            <div className='albumCover'><img src={playlist.picture_xl} alt='' />
-            <div className='albumName'>{playlist.title} </div>
-            </div>
-            </div>
-
-            <div className='artistNameDiv'>{playlist.title}</div>
-</div>
-
-        )
-    })}
+    return (
+        <div className='artistsContainer'>
+            <LoggedInNav />
+            <div className='artistsBody'>
+                <div className='header'>Playlists</div>
+                <div className='topArtistsContainer'>
+                    {playlists && playlists.map(playlist => {
+                        return (
+                            <div key={playlist.id}>
+                                <Modal show={show} onClose={() => setShow(false)} title='Playlists'>
+                                    <div className='closeBtn'><div onClick={() => setShow(false)} className='closeContainer'><img className='closeImg' src={cancelButton} alt='' /></div></div>
+                                    <div className='playlistModal-title'>{select}</div>
+                                    <div className='playlistModal-createDiv'>buttons</div>
+                                    {!loader ? playlistData.map(track => {
+                                        return (
+                                            <div key={track.id} className='childrenContainer'>
 
 
-</div>
-        {/* <div class='header secondHeader'>Charts</div>
+                                                <div className='playlistModal-body'>
+                                                    <div className='modalListDiv'><img className='deleteImg' src={deleteButton} alt='' /><img className='twndimg' src={track.artist.picture_xl} alt='' /></div> <div className='playListModalTittle'><div>{track.title}</div> <div>{track.artist.name}</div></div> <div className='playlistPlayDiv'><img className='playImg' src={play} onClick={() => { setTrackId(track.preview); player() }} alt='' /> <img className='playlistLogoImg' src={playlistLogo} alt='' /></div>
+
+                                                </div>
+                                            </div>
+                                        )
+                                    }) : <div className='spinnerContainer'> <div className="lds-facebook"><div></div><div></div><div></div></div> </div>}
+
+                                </Modal>
+                                <div data-aos='fade-up' className='artistImgContainer' onClick={() => { setShow(true); setSelect(playlist.title); settracklist(playlist.tracklist); dispatch(fetchPlaylist(playlist.tracklist)) }}>
+                                    <img src={playlist.picture_xl} alt='' />
+                                    <div className='albumCover'><img src={playlist.picture_xl} alt='' />
+                                        <div className='albumName'>{playlist.title} </div>
+                                    </div>
+                                </div>
+
+                                <div className='artistNameDiv'>{playlist.title}</div>
+                            </div>
+
+                        )
+                    })}
+
+
+                </div>
+                {/* <div class='header secondHeader'>Charts</div>
 <div className='topArtistsContainer'>
             { charts && charts.map(chart =>{
         return(
@@ -122,10 +137,10 @@ return (
 </div> */}
 
 
-    </div>
-</div>
-)
+            </div>
+        </div>
+    )
 
-   }
+}
 
-   export default Playlists
+export default Playlists
