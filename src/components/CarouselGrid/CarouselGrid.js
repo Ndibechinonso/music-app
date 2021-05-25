8f163b5202320946ec69400f90f41fefb718387d
Ndibechinonso <edwardndibe@gmail.com>
2021-05-25 22:03:32 +0100

    carousel

--- src/components/CarouselGrid/CarouselGrid.js
+++ src/components/CarouselGrid/CarouselGrid.js
@@ -28,22 +28,20 @@ if(fetchedData2){
 <div className='carouselGridContainer'>
 <div className='artistsBody'>
     <div className='lastPlayedheader'>Last played songs</div>
-        <div style={{ maxWidth: 1200, marginLeft: 'auto', marginRight: 'auto', marginTop: 20 }}>
+        <div style={{ maxWidth: 1200, marginLeft: 'auto', marginRight: 'auto', marginTop: 20 }} className='test'>
             <SecondCarousel show={5} >
 
                 { loader === true ? <div className='spinnerContainer'> <div className="lds-facebook"><div></div><div></div><div></div></div> </div> : lastPlayed ? lastPlayed.map(data => {
-                    return (
-                        <div key={data.artist.id + nanoid()} data-aos="fade-left">
-
-                            <div >
-                                <div className='slideContainer' style={{ padding: 8 }}>  
-                               <div className='imgContainer'>   <img className='roundedImg' src={data.album.cover_xl} alt="placeholder" style={{ width: '100%' }} /></div>
-                             <div className='titleDiv'>
-                                <p>{data.title}</p>
-                                <p className='artistName'>{data.artist.name}</p></div>
+                  return (
+                        <div key={data.artist.id + nanoid()}  className='testKid'>
+                                <div className='testKid' style={{ padding: 8 }} >  
+                               <div className='imgContainer'>   <img className='roundedImg' src={data.album.cover_xl} alt="placeholder" style={{ width: '100%' }} />
+                               <div className='titleDiv'>
+                                <p className='trackName'>{data.title}</p>
+                                <p className='artistName'>{data.artist.name}</p>
                                 </div>
-                            </div>
-
+                                </div>
+                                </div>
                         </div>
                     )
                 }) : <div className='requesFailed'>FAILED, KINDLY RESTART APP</div>
@@ -55,22 +53,20 @@ if(fetchedData2){
 
 <div className='latestTracksBody'>
         <div className='lastPlayedheader'>Latest tracks</div>
-        <div style={{ maxWidth: 1200, marginLeft: 'auto', marginRight: 'auto', marginTop: 20 }}>
+        <div style={{ maxWidth: 1200, marginLeft: 'auto', marginRight: 'auto', marginTop: 20 }} className='test'>
             <SecondCarousel show={5} >
-
                 { loader === true ? <div className='spinnerContainer'> <div className="lds-facebook"><div></div><div></div><div></div></div> </div> : latestTracks ? latestTracks.map(track => {
                     return (
-                        <div key={track.id + nanoid()} data-aos="fade-left">
-
-                            <div >
-                                <div className='slideContainer' style={{ padding: 8 }}>  
-                               <div className='imgContainer'>   <img className='roundedImg' src={track.cover_xl} alt="placeholder" style={{ width: '100%' }} /></div>
+                        <div key={track.id + nanoid()} data-aos="fade-left"  className='testKid'>
+                                <div className='testKid' style={{ padding: 8 }}>  
+                               <div className='imgContainer'>   <img className='roundedImg' src={track.cover_xl} alt="placeholder" style={{ width: '100%' }} />
                              <div className='titleDiv'>
                                 <p>{track.title}</p>
-                                <p className='artistName'>{track.artist.name}</p></div>
+                                <p className='artistName'>{track.artist.name}</p>
                                 </div>
-                            </div>
-
+                                </div>
+                            
+                                </div>
                         </div>
                     )
                 }) : <div className='requesFailed'>FAILED, KINDLY RESTART APP</div>
