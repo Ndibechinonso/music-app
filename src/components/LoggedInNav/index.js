62e64f644fa57e47275c61562ceb98b0e848f0d1
Ndibechinonso <edwardndibe@gmail.com>
2021-05-27 18:53:19 +0100

    navlink

--- src/components/LoggedInNav/index.js
+++ src/components/LoggedInNav/index.js
@@ -16,6 +16,7 @@ import deezifylogo from '../../Assets/deezifylogo.png'
 import autoplayoff from '../../Assets/autoplayoff.png'
 import autoplayon from '../../Assets/autoplayon.png'
 import feedback from '../../Assets/feedback.png'
+import { NavLink } from "react-router-dom";
 
 
 function LoggedInNav(props) {
@@ -24,6 +25,7 @@ function LoggedInNav(props) {
   const [accountlink, setaccountlink] = useState(false)
   const [autoPlay, setAutoPlay] = useState(false)
   const [isSubmitted, setIsSubmitted] = useState(false)
+  const [isOpen, setOpen] = useState(false);
 
   function accountDrop() {
     setaccountlink(true)
@@ -80,7 +82,7 @@ function LoggedInNav(props) {
       })
       ,
       onSubmit: values => {
-          
+
              axios.post('https://music-app-feeder.herokuapp.com/feedback', { fullName: values.fullName, email: values.email, feedback: values.feedback })
              .then(response => {
               const responseInfo = response.message
@@ -95,83 +97,112 @@ function LoggedInNav(props) {
         },
       });
   
-      return (
-        <div>
-          {!isSubmitted ? <form onSubmit={handleSubmit}>
-            <div className='closeBtn'><div onClick={() => setShow(false)} className='closeContainer'><img className='closeImg' src={cancelButton} alt='' /></div></div>
-            <div className='feedbackModal-title'> We appreciate your feedback</div>
-            <p className='feedBackText'>Please leave a message for us and we will reach out to you as soon as possible</p>
-  
-            <div className='inputDiv'>
-              <div className='inputGroup'>
-                <label htmlFor="firstName">Full Name</label><br />
-                <input
-                  className='userInfo'
-                  id="fullName"
-                  name="fullName"
-                  type="text"
-                  onChange={e => setFieldValue("fullName", e.target.value)}
-                  value={values.fullName}
-                /><br />
-                {errors.fullName ? <div className='error'>{errors.fullName}</div> : null}
-              </div>
-  
-              <div className='inputGroup'>
-                <label htmlFor="email">Email Address</label><br />
-                <input
-                  className='userInfo'
-                  id="email"
-                  name="email"
-                  type="email"
-                  onChange={e => setFieldValue("email", e.target.value)}
-                  value={values.email}
-                /><br />
-                {errors.email ? <div className='error'>{errors.email}</div> : null}
-              </div>
-  
-              <div className='inputGroup'>
-                <label htmlFor="feedback">Feedback</label><br />
-                <textarea
-                  className='userInfo'
-                  id="feedback"
-                  name="feedback"
-                  type="text"
-                  onChange={e => setFieldValue("feedback", e.target.value)}
-                  value={values.feedback}
-                /><br />
-                {errors.feedback ? <div className='error'>{errors.feedback}</div> : null}
-              </div>
-            </div>
-  
-            <div className='buttonDiv'> <Button className='feedbackButton' text='Submit' type='submit' /></div>
-          </form> : <div><div className='closeBtn'><div onClick={() => {setShow(false); setIsSubmitted(false)}} className='closeContainer'><img className='closeImg' src={cancelButton} alt='' /></div></div>
-            <div className='feedbackClass'><div><img src={feedback} /></div><p>Thank you for sharing this with us. We will get back to you as soon as possible.</p><p>Till then, Keep streamiing!!!</p></div></div>}
-        </div>);
-    };
-    return (
+     return (
       <div>
-        <Modal show={show} onClose={() => {setShow(false); setIsSubmitted(false)}} >
-          {SignupForm()}
-        </Modal>
-  
-        <div className='loggedNavBar'>
-          <Link to='/home'> <h2><img src={deezifylogo} /></h2></Link>
-          <ul className='loggedNavBar-links'>
-            <Link to='/home'> <li className='' >Home</li></Link> <Link to='/artists'><li>Artists </li></Link> <Link to='/playlists'><li>PlayLists </li></Link> <Link to='/genres'> <li>Genres </li> </Link> <li onClick={accountDrop}><img src={dropdown} alt='dropdown icon' /></li>
-          </ul>
-        </div>
-  
-        { accountlink ? <div className='accountModal'>
-          <div className='modal-content' onClick={e => e.stopPropagation()}>
-            {userData ? <div className='settings'><img src={userData.picture_xl} className='user userImg' alt='' /><p>{userData.name}</p></div>
-              : <div className='settings'> <i className="fas userIcon fa-user-circle"></i> User</div>}
-            <div className='settings' onClick={() => { setShow(true); setaccountlink(false) }}><img src={icon} className='user' alt='feedback icon' /> Send Feedback</div>
-            <div className='settings'><img src={icon2} className='user' alt='autoplay icon' /> Auto play on hover <div className='autoplayDiv'>{autoPlay ? <img src={autoplayon} onClick={() => setAutoPlay(false)} /> : <img src={autoplayoff} onClick={() => setAutoPlay(true)} />} </div> </div>
-            <Link to='/'><div className='settings'><img src={icon3} className='user' alt='logout icon' /> Log out</div></Link>
+        {!isSubmitted ? <form onSubmit={handleSubmit}>
+          <div className='closeBtn'><div onClick={() => setShow(false)} className='closeContainer'><img className='closeImg' src={cancelButton} alt='' /></div></div>
+          <div className='feedbackModal-title'> We appreciate your feedback</div>
+          <p className='feedBackText'>Please leave a message for us and we will reach out to you as soon as possible</p>
+
+          <div className='inputDiv'>
+            <div className='inputGroup'>
+              <label htmlFor="firstName">Full Name</label><br />
+              <input
+                className='userInfo'
+                id="fullName"
+                name="fullName"
+                type="text"
+                onChange={e => setFieldValue("fullName", e.target.value)}
+                value={values.fullName}
+              /><br />
+              {errors.fullName ? <div className='error'>{errors.fullName}</div> : null}
+            </div>
+
+            <div className='inputGroup'>
+              <label htmlFor="email">Email Address</label><br />
+              <input
+                className='userInfo'
+                id="email"
+                name="email"
+                type="email"
+                onChange={e => setFieldValue("email", e.target.value)}
+                value={values.email}
+              /><br />
+              {errors.email ? <div className='error'>{errors.email}</div> : null}
+            </div>
+
+            <div className='inputGroup'>
+              <label htmlFor="feedback">Feedback</label><br />
+              <textarea
+                className='userInfo'
+                id="feedback"
+                name="feedback"
+                type="text"
+                onChange={e => setFieldValue("feedback", e.target.value)}
+                value={values.feedback}
+              /><br />
+              {errors.feedback ? <div className='error'>{errors.feedback}</div> : null}
+            </div>
           </div>
-        </div> : null}
+
+          <div className='buttonDiv'> <Button className='feedbackButton' text='Submit' type='submit' /></div>
+        </form> : <div><div className='closeBtn'><div onClick={() => {setShow(false); setIsSubmitted(false)}} className='closeContainer'><img className='closeImg' src={cancelButton} alt='' /></div></div>
+          <div className='feedbackClass'><div><img src={feedback} /></div><p>Thank you for sharing this with us. We will get back to you as soon as possible.</p><p>Till then, Keep streamiing!!!</p></div></div>}
+      </div>);
+  };
+  return (
+    <div>
+      <Modal show={show} onClose={() => {setShow(false); setIsSubmitted(false)}} >
+        {SignupForm()}
+      </Modal>
+
+      <div className='loggedNavBar'>
+        <Link to='/home'> <h2><img src={deezifylogo} /></h2></Link>
+        <ul className='loggedNavBar-links'>
+     
+            <NavLink className="navLink" activeClassName="is-active" to="/home">
+              Home
+            </NavLink>
+
+            <NavLink
+              className="navLink"
+              activeClassName="is-active"
+              to="/artists"
+            >
+              Artists
+            </NavLink>
+
+            <NavLink
+              className="navLink"
+              activeClassName="is-active"
+              to="/playlists"
+            >
+              PlayLists
+            </NavLink>
+          
+          <NavLink
+              className="navLink"
+              activeClassName="is-active"
+              to="/genres"
+            >
+              Genres
+            </NavLink>
+       
+            <div className='dropdownDiv'>{userData ? <img src={userData.picture_xl} className='user userImg' alt='' /> : null} <li onClick={accountDrop}><img src={dropdown} alt='dropdown icon' className='dropdownIcon'/></li></div>
+        </ul>
       </div>
-    )
-  }
-  
-  export default LoggedInNav
+
+      { accountlink ? <div className='accountModal'>
+        <div className='modal-content' onClick={e => e.stopPropagation()}>
+          {userData ? <div className='settings'><img src={userData.picture_xl} className='user userImg' alt='' /><p>{userData.name}</p></div>
+            : <div className='settings'> <i className="fas userIcon fa-user-circle"></i> User</div>}
+          <div className='settings' onClick={() => { setShow(true); setaccountlink(false) }}><img src={icon} className='user' alt='feedback icon' /> Send Feedback</div>
+          <div className='settings'><img src={icon2} className='user' alt='autoplay icon' /> Auto play on hover <div className='autoplayDiv'>{autoPlay ? <img src={autoplayon} onClick={() => setAutoPlay(false)} /> : <img src={autoplayoff} onClick={() => setAutoPlay(true)} />} </div> </div>
+          <Link to='/'><div className='settings'><img src={icon3} className='user' alt='logout icon' /> Log out</div></Link>
+        </div>
+      </div> : null}
+    </div>
+  )
+}
+
+export default LoggedInNav
