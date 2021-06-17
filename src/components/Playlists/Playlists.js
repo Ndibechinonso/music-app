7ea481f9477f11e4e647e878d6fb4dcd35db1119
Ndibechinonso <edwardndibe@gmail.com>
2021-06-17 09:18:11 +0100

    refresh playlist on add

--- src/components/Playlists/Playlists.js
+++ src/components/Playlists/Playlists.js
@@ -145,6 +145,7 @@ const Playlists = (props) => {
         setPlaylistCreateAlert(true);
         clearPlaylistCreateAlert();
         setplaylistName("");
+        setCreatePlayListModal(false)
         dispatch(fetchPlaylistsPageData())
       })
       .catch((error) => {
